fd01d11b6bd6cb1695c2c8fe8b639cd9df24c883
Adrian Feeger <139186297+adrianRfeeger@users.noreply.github.com>
2024-07-02 20:01:13 +1000

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
-from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QProgressBar, QFileDialog, QMessageBox
+from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QProgressBar, QFileDialog, QCheckBox
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
 import configparser
 import os
 import sys
 import pathlib
+import subprocess
 from ezshare import EZShare
 from wifi import connect_to_wifi, disconnect_from_wifi
 
@@ -23,7 +24,7 @@ def shorten_path(path):
 
 class EzShareWorker(QThread):
     progress = pyqtSignal(int)
-    status = pyqtSignal(str)
+    status = pyqtSignal(str, str)  # Added second parameter for message type (info/error)
     finished = pyqtSignal()
 
     def __init__(self, ezshare):
@@ -38,7 +39,7 @@ class EzShareWorker(QThread):
             connect_to_wifi(self.ezshare)  # Connect to Wi-Fi before starting the process
             self.ezshare.run()
         except RuntimeError as e:
-            self.update_status(f'Error: {e}')
+            self.update_status(f'Error: {e}', 'error')
         finally:
             disconnect_from_wifi(self.ezshare)  # Disconnect from Wi-Fi after the process completes
             self.finished.emit()
@@ -46,8 +47,8 @@ class EzShareWorker(QThread):
     def update_progress(self, value):
         self.progress.emit(value)
 
-    def update_status(self, message):
-        self.status.emit(message)
+    def update_status(self, message, message_type='info'):
+        self.status.emit(message, message_type)
 
     def stop(self):
         self._is_running = False
@@ -108,6 +109,12 @@ class EzShareCPAP(QMainWindow):
         psk_layout.addWidget(self.psk_label)
         psk_layout.addWidget(self.psk_entry)
 
+        # Checkboxes
+        self.import_oscar_checkbox = QCheckBox("Import with OSCAR after completion")
+        self.import_oscar_checkbox.setChecked(self.config['Settings'].getboolean('import_oscar', False))
+        self.quit_checkbox = QCheckBox("Quit after completion")
+        self.quit_checkbox.setChecked(self.config['Settings'].getboolean('quit_after_completion', False))
+
         # Buttons
         btn_layout = QHBoxLayout()
         self.start_btn = QPushButton('Start')
@@ -118,10 +125,13 @@ class EzShareCPAP(QMainWindow):
         self.default_btn.clicked.connect(self.restore_defaults)
         self.cancel_btn = QPushButton('Cancel')
         self.cancel_btn.clicked.connect(self.cancel_process)
+        self.quit_btn = QPushButton('Quit')
+        self.quit_btn.clicked.connect(self.close)
         btn_layout.addWidget(self.start_btn)
         btn_layout.addWidget(self.save_btn)
         btn_layout.addWidget(self.default_btn)
         btn_layout.addWidget(self.cancel_btn)
+        btn_layout.addWidget(self.quit_btn)
 
         # Progress bar
         self.progress_bar = QProgressBar()
@@ -129,13 +139,15 @@ class EzShareCPAP(QMainWindow):
         self.progress_bar.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         # Status label
-        self.status_label = QLabel()
+        self.status_label = QLabel("Ready.")
         self.status_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         layout.addLayout(path_layout)
         layout.addLayout(url_layout)
         layout.addLayout(ssid_layout)
         layout.addLayout(psk_layout)
+        layout.addWidget(self.import_oscar_checkbox)
+        layout.addWidget(self.quit_checkbox)
         layout.addLayout(btn_layout)
         layout.addWidget(self.progress_bar)
         layout.addWidget(self.status_label)
@@ -158,17 +170,19 @@ class EzShareCPAP(QMainWindow):
         psk = self.psk_entry.text()
 
         if not path or not url or not ssid:
-            QMessageBox.warning(self, 'Input Error', 'All fields must be filled out.')
+            self.update_status('Input Error: All fields must be filled out.', 'error')
             return
 
         if not pathlib.Path(path).expanduser().is_dir():
-            QMessageBox.warning(self, 'Invalid Path', 'The specified path does not exist or is not a directory.')
+            self.update_status('Invalid Path: The specified path does not exist or is not a directory.', 'error')
             return
 
         self.config['Settings']['path'] = path
         self.config['Settings']['url'] = url
         self.config['WiFi']['ssid'] = ssid
         self.config['WiFi']['psk'] = psk
+        self.config['Settings']['import_oscar'] = str(self.import_oscar_checkbox.isChecked())
+        self.config['Settings']['quit_after_completion'] = str(self.quit_checkbox.isChecked())
 
         self.ezshare.set_params(
             path=path,
@@ -199,31 +213,48 @@ class EzShareCPAP(QMainWindow):
     def update_progress(self, value):
         self.progress_bar.setValue(value)
 
-    def update_status(self, message):
+    def update_status(self, message, message_type='info'):
+        if message_type == 'error':
+            self.status_label.setStyleSheet("color: red;")
+        else:
+            self.status_label.setStyleSheet("")  # Reset to default color
         self.status_label.setText(message)
-        if 'Error' in message:
-            QMessageBox.critical(self, 'Error', message)
 
     def process_finished(self):
-        QMessageBox.information(self, 'Process Completed', 'The process has completed successfully.')
-        self.progress_bar.setValue(100)
-        self.status_label.setText('Process completed.')
-        # Reset progress bar after showing completion status
+        self.update_status('Ready.', 'info')
         self.progress_bar.setValue(0)
-        self.status_label.setText('Ready.')
+        if self.import_oscar_checkbox.isChecked():
+            self.import_cpap_data_with_oscar()
+        if self.quit_checkbox.isChecked():
+            self.close()
+
+    def import_cpap_data_with_oscar(self):
+        script = '''
+        tell application "OSCAR"
+            activate
+            delay 2
+            tell application "System Events"
+                tell process "OSCAR"
+                    click menu item "Import CPAP Card Data" of menu "File" of menu bar 1
+                end tell
+            end tell
+        end tell
+        '''
+        subprocess.run(["osascript", "-e", script])
 
     def cancel_process(self):
         if self.worker and self.worker.isRunning():
             self.worker.stop()
             self.worker.wait()  # Ensure the thread finishes properly
             self.progress_bar.setValue(0)
-            self.status_label.setText('Process cancelled.')
-            QMessageBox.information(self, 'Process Cancelled', 'The process has been cancelled.')
+            self.update_status('Process cancelled.', 'info')
 
     def closeEvent(self, event):
         if self.worker and self.worker.isRunning():
             self.worker.stop()
             self.worker.wait()
+        self.update_status('Ready.', 'info')
+        self.progress_bar.setValue(0)
         event.accept()
 
     def load_default_config(self):
@@ -239,7 +270,9 @@ class EzShareCPAP(QMainWindow):
             'ignore': '',
             'retries': '3',
             'connection_delay': '5',
-            'debug': 'true'
+            'debug': 'true',
+            'import_oscar': 'false',
+            'quit_after_completion': 'false'
         }
         default_config['WiFi'] = {
             'ssid': 'ez Share',
@@ -258,9 +291,11 @@ class EzShareCPAP(QMainWindow):
         self.config['Settings']['url'] = self.url_entry.text()
         self.config['WiFi']['ssid'] = self.ssid_entry.text()
         self.config['WiFi']['psk'] = self.psk_entry.text()
+        self.config['Settings']['import_oscar'] = str(self.import_oscar_checkbox.isChecked())
+        self.config['Settings']['quit_after_completion'] = str(self.quit_checkbox.isChecked())
         with open(self.config_file, 'w') as configfile:
             self.config.write(configfile)
-        QMessageBox.information(self, 'Settings Saved', 'Settings have been saved successfully.')
+        self.update_status('Settings have been saved successfully.', 'info')
 
     def restore_defaults(self):
         self.config = self.default_config
@@ -268,7 +303,9 @@ class EzShareCPAP(QMainWindow):
         self.url_entry.setText(self.config['Settings']['url'])
         self.ssid_entry.setText(self.config['WiFi']['ssid'])
         self.psk_entry.setText(self.config['WiFi']['psk'])
-        QMessageBox.information(self, 'Defaults Restored', 'Settings have been restored to defaults.')
+        self.import_oscar_checkbox.setChecked(self.config['Settings'].getboolean('import_oscar', False))
+        self.quit_checkbox.setChecked(self.config['Settings'].getboolean('quit_after_completion', False))
+        self.update_status('Settings have been restored to defaults.', 'info')
 
 if __name__ == "__main__":
     import sys
